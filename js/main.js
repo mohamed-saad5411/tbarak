c88ab749bcb61ccd0aec61179978a48f07e0a400
mohamed saad <124407370+mohamed-saad5411@users.noreply.github.com>
2025-03-08 02:29:54 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -42,3 +42,58 @@ $('#structure').hover(function () {
     $(".structure").slideUp(600)
 }
 );
+
+$('#contact a').hover(function () {
+    // over
+}
+);
+$('#contact').hover(function () {
+    // over
+}
+);
+
+$('#openNav').click(function () {
+    $("#navList").fadeToggle(600);
+    document.getElementById('navList').classList.replace('d-none','d-block')
+}
+);
+
+$('#closeNav').click(function () {
+    $("#navList").fadeToggle(1000);
+    document.getElementById('navList').classList.replace('d-block','d-none')
+}
+);
+
+
+// $(selector).click(function (e) { 
+//     e.preventDefault();
+    
+// });
+
+$('#contact a').click(function () { 
+    $("#contact ul").slideToggle(600)
+    $("#operation ul").slideUp(600)
+    $("#infra ul").slideUp(600)
+    $("#structure ul").slideUp(600)
+    
+});
+$('#operation a').click(function () { 
+    $("#operation ul").slideToggle(600)
+    $("#contact ul").slideUp(600)
+    $("#infra ul").slideUp(600)
+    $("#structure ul").slideUp(600)
+});
+$('#infra a').click(function () { 
+    $("#infra ul").slideToggle(600)
+    $("#contact ul").slideUp(600)
+    $("#operation ul").slideUp(600)
+    $("#structure ul").slideUp(600)
+});
+$('#structure a').click(function () { 
+    $("#structure ul").slideToggle(600)
+    $("#contact ul").slideUp(600)
+    $("#infra ul").slideUp(600)
+    $("#operation ul").slideUp(600)
+});
+
+
